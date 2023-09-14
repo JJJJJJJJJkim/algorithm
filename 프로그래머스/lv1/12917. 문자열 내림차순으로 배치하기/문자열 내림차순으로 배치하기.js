f3ae1f69f98261e1ef8695db7cf883719654b2d1
Tiger Water <133957930+JJJJJJJJJkim@users.noreply.github.com>
2023-09-14 18:39:03 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
+++ 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -4,7 +4,6 @@ function solution(s) {
             .sort((a, b) => {
                 if (a < b) return 1;
                 if (a > b) return -1;
-                return 0;
             })
             .join('');
     return answer;
